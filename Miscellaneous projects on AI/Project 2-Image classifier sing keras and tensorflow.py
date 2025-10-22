0d574edb687b337ed6562231c21c4723f67a811d
VrishankDesai <vrishankdesai20@gmail.com>
2025-10-22 15:47:26 +0530

    Add files via upload

---
+++ Miscellaneous projects on AI/Project 2-Image classifier sing keras and tensorflow.py
@@ -0,0 +1,61 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+from tensorflow.keras.utils import to_categorical
+import matplotlib.pyplot as plt
+
+#Load mnist dataset-contains train images and test images
+(train_images, train_labels), (test_images,test_labels)= datasets.mnist.load_data()
+
+#Preprocess the data-makes training faster and efficient
+train_images = train_images / 255.0 #Normalize pixel values to be between 0 and 1 which is why it is divided by 255 to scale values between 0 and 1
+test_images = test_images / 255.0
+
+#Reshape images to (28, 28, 1) as they are grayscale images-mnist images are greyscale which have only 1 channel
+#28,28,1 represents height, width and colour channels respectively
+train_images = train_images.reshape((train_images.shape[0],28,28,1))
+test_images = test_images.reshape((test_images.shape[0],28,28,1))
+
+#Convert labels to one hot encoded format
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
+
+#Build the CNN model
+model = models.Sequential()
+
+#First convoltional layer
+model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(28,28,1))) #32 is number of convolutional filters, (3,3) is the size of the pixels,activation function sets nregative pixel values to 0
+model.add(layers.MaxPooling2D((2,2)))
+
+#Second convolutional layer
+model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+
+#Third convolutional layer
+model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+
+#Flatten the output to 1D and feed it into dense layers,converts 3d output to 1d vector
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu')) #Fully connected layer with 64
+
+#Output layer with 10 neurons for 10 classes
+model.add(layers.Dense(10, activation='softmax')) #Softmax for multi-class
+
+#Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+#Train the model
+model.fit(train_images, train_labels, epochs=5, batch_size=64, validation_split=0.2)
+
+#Evaluate the model on test data
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print(f"Test accuracy: {test_acc*100:.2f}%")
+
+#Make predictions on test images
+predictions = model.predict(test_images)
+print(f"Prediction for first test image: {np.argmax(predictions[4])}") 
+
+plt.imshow(test_images[0].reshape(28,28), cmap='gray')
+plt.title(f"Predicted Label: {np.argmax(predictions[4])}")
+plt.show()
